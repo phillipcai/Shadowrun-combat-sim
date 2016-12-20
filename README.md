a77043be4c3d883cfb1fe91d23e21004521fccc4
Young Li <li.young1618@gmail.com>
2016-12-19 18:35:47 -0800

    Test stuff using python gtk3 tutorial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Shadowrun-combat-sim
+
+A combat simulator for Shadowrun 4th Edition using GTK+ and written in Python 3.
+
---
+++ scs.py
@@ -0,0 +1,6 @@
+from gi.repository import Gtk
+import sys
+
+class SCS(Gtk.Application):
+    def do_activate(self):
+        # create a GTK
